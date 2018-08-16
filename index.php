6a153e7f632be0688e21c83c821423b4db9c6d97
Driss <driss@mac-mini-de-driss.home>
2018-08-16 18:42:22 +0200

    First Commit Of My Super Duper Git Website

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My Super Duper Git Website</title>
+  </head>
+  <body>
+    <h1><?php echo "Hi Humans I'm a Super Duper Git Website"; ?></h1>
+  </body>
+</html>
